8283f92f37e17e9107b2fa339c7a18e36fc2a7e7
daniel-s-cunha <dan.cunha.s@gmail.com>
2026-05-30 15:03:01 -0400

    changing loss back to predictive distribution

--- sim_study_batch.py
+++ sim_study_batch.py
@@ -69,7 +69,7 @@ def main():
         # 2. Initialize and Fit Model
         # Note: Ensure block_sz matches your init parameter name (block_sz vs block_sz)
         mod = sofm.SOFM(da, n_components=3, max_lag = max_lag, nonstationary=True, block_sz=1, n_blocks=int(sq_len**2/10),n_cores=1)
-        mod.fit(lss = [1,3,5],phis=[10**j for j in range(5)]+[5**j for j in range(5)],rots=[0])#[0,np.pi/8,2*np.pi/8,3*np.pi/8])
+        mod.fit(lss = [1,3,5],phis=[10**j for j in range(5)]+[5*10**j for j in range(5)],rots=[0])#[0,np.pi/8,2*np.pi/8,3*np.pi/8])
         sc = mod.spatcov_
         if prior_covs_n[prior_cov]=='independent':
             lat_mse = 0
